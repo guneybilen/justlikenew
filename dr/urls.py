5c87dab2450a055db71b4e2a474a21a48d76d82c
guney bilen <guneybilen@yahoo.com>
2022-01-31 19:26:20 -0500

    gunicorn install

--- dr/urls.py
+++ dr/urls.py
@@ -27,4 +27,8 @@ urlpatterns = [
       # path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
       # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
       # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
