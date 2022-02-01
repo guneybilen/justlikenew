3d0aefdac94d0a41567d488a658bca54fe6f89d4
guney bilen <guneybilen@yahoo.com>
2022-02-01 08:38:02 -0500

    user model updated for two print statements

--- users/models.py
+++ users/models.py
@@ -39,7 +39,7 @@ class CustomUserManager(BaseUserManager):
         Create and save a SuperUser with the given email and password.
         """
 
-        print("superuser created")
+        print("superuser created...")
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
